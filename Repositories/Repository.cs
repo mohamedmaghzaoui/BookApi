61567efc16ab9325721b1e165ecb92855976d166
mohamedmaghzaoui <mohamedmaghzaoui53@gmail.com>
2025-06-11 10:20:09 +0200

    added missed }

--- Repositories/Repository.cs
+++ Repositories/Repository.cs
@@ -9,31 +9,29 @@ namespace Repositories
     {
         private readonly BookContext _context;
         private readonly DbSet<T> dbTable;
-        
+
 
         public Repository(BookContext context)
         {
-            
+
             _context = context;
-            dbTable = context.Set<T>();  
+            dbTable = context.Set<T>();
         }
         //add a new item to db
         public void Add(T newEntity)
         {
             dbTable.Add(newEntity);
-         
+
             _context.SaveChanges();
         }
         //delete a item 
         public void Delete(object id)
         {
             T entity = dbTable.Find(id);
-            
-            
 
             dbTable.Remove(entity);
             _context.SaveChanges();
-            
+
         }
         //list all items
         public List<T> GetAll()
@@ -56,3 +54,4 @@ namespace Repositories
 
         }
     }
+}
